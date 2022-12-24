bcd031c72386730b3430286d9e50fed204c04b3b
witalijkrajewski <witalijkrajewski@gmail.com>
2022-12-24 10:20:08 +0300

    FCS-14 - Blog serializers created.

---
+++ backend/blog/serializes.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import BlogMainTheme, Blog
+
+
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+
+        fields = (
+            'id',
+            'theme',
+            'title',
+            'text',
+            'get_blog_image',
+            'get_blog_thumbnail',
+            'date_created',
+        )
+
+
+class MainThemeSerializer(serializers.ModelSerializer):
+    blogs = BlogSerializer(many=True)
+
+    class Meta:
+        model = BlogMainTheme
+
+        fields = (
+            'id',
+            'main_theme',
+            'blogs',
+            'get_absolute_url'
+        )
+
